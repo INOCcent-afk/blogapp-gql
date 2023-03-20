816cbf2fd00c4036c5291a85403b820dfdc86273
dave <michael828inoc@gmail.com>
2023-03-20 14:19:21 +0800

    n + 1

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -1,8 +1,27 @@
 import { Context } from "..";
 
 export const Query = {
+	me: async (_: any, __: any, { userInfo, prisma }: Context) => {
+		if (!userInfo) return null;
+
+		return prisma.user.findUnique({
+			where: {
+				id: userInfo.userId,
+			},
+		});
+	},
+	profile: (_: any, { userId }: { userId: string }, { prisma }: Context) => {
+		return prisma.profile.findUnique({
+			where: {
+				userId: Number(userId),
+			},
+		});
+	},
 	posts: (_: any, __: any, { prisma }: Context) => {
 		return prisma.post.findMany({
+			where: {
+				published: true,
+			},
 			orderBy: [
 				{
 					createdAt: "desc",
